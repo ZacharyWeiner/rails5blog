a9751133bd2b616c799ce11f3e21258ef4e8dfd3
ZacharyWeiner <zach@cloud.com>
2017-06-26 13:55:39 -0500

    added twitter call on tech news page

--- config/routes.rb
+++ config/routes.rb
@@ -7,11 +7,9 @@ Rails.application.routes.draw do
   get 'angular-items', to: 'portfolios#angular'
   get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
   get 'pages/home'
-
   get 'pages/about'
-
   get 'pages/contact'
-
+  get 'tech-news', to: 'pages#tech_news'
   resources :blogs do 
     member do 
       get :toggle_status
