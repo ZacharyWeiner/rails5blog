6cf16ee5db98c0040798f36cf5c1207976f06e0f
ZacharyWeiner <zach@cloud.com>
2017-06-26 11:45:23 -0500

    refactored alerts into helper methods

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -39,4 +39,15 @@ module ApplicationHelper
   def active? path
     "active" if current_page? path
   end
+
+  def alert_generator msg
+    js add_gritter(msg, title: "Zack Weiners Awesomeness", sticky: false)
+  end
+
+  def alerts
+    alert = (flash[:alert] || flash[:error] || flash[:notice])
+    if alert
+      alert_generator alert
+    end
+  end
 end
